51e37d54b2a4d13c948def5e293e39ccd22d9894
jialin <shujialin@live.com>
2025-10-25 17:17:01 +0800

    使用net9.0以适配jellyfin 10.11，修改项目文件结构

--- Plugin.cs
+++ Plugin.cs
@@ -10,7 +10,7 @@ namespace StrmTool
     {
         public static Plugin Instance { get; private set; }
         public static string PluginName = "Strm Tool";
-        private Guid _id = new Guid("6107fc8c-883a-4171-b70e-7590658706d8");
+        private Guid _id = new Guid("6107fc8c-883a-4171-b70e-7590658706B9");
 
         public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer) : base(applicationPaths, xmlSerializer)
         {
